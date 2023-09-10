4f8c161f460142f952a7e56e82243719b61c061e
João Scaleão <scaleao123@gmail.com>
2023-09-10 16:55:01 -0300

    add build

---
+++ dist/precache-manifest.b477d312bb640697ebfbd8cc2895c75d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee23402a628af63f9395",
+    "url": "/css/app.9ed2a495.css"
+  },
+  {
+    "revision": "c91d2c0d5ce0e699e286",
+    "url": "/css/chunk-d4fdf568.5ef1afaa.css"
+  },
+  {
+    "revision": "c6e5139ed2e7282afd14",
+    "url": "/css/chunk-vendors.f3d3c6ad.css"
+  },
+  {
+    "revision": "49f2e1a9a8773894fe6d04032611fad6",
+    "url": "/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.49f2e1a9.woff"
+  },
+  {
+    "revision": "37ce905ae8ba1e0d17c9ba681e6a0af6",
+    "url": "/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.37ce905a.woff2"
+  },
+  {
+    "revision": "3e77db3e3658fc76cefc022e2f79a0b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee23402a628af63f9395",
+    "url": "/js/app.9aa09304.js"
+  },
+  {
+    "revision": "c91d2c0d5ce0e699e286",
+    "url": "/js/chunk-d4fdf568.aee7cc04.js"
+  },
+  {
+    "revision": "c6e5139ed2e7282afd14",
+    "url": "/js/chunk-vendors.4de53cb9.js"
+  },
+  {
+    "revision": "82844b3db80541424433005f4f6bc36e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
